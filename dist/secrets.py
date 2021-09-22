2e7e373542d3e662adb0819afae0415ae9a95d8d
OneOfTheInfiniteMonkeys <45082871+OneOfTheInfiniteMonkeys@users.noreply.github.com>
2021-09-22 19:25:05 +0100

    Empty secrets.py
    Added empty secrets.py - Ensure local backup made before deployment

---
+++ dist/secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+     
+secrets = {
+    'ssid' : '',
+    'password' : '',
+    'aio_username' : '',
+    'aio_key' : '',
+    'timezone' : "", # http://worldtimeapi.org/timezones
+    }
